f71118b7c4942f8f42cd383e8a1d1e017b5ccce1
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-08-17 17:31:48 -0400

    First commit

---
+++ eManager.Domain/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eManager.Domain
+{
+    public class Department
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+
+
+    interface ICustomer
+    {
+        void Print();
+    }
+
+}
